b123c22f306644c4c3ce37051fad460cf368cc1d
Anton <capitalsav@gmail.com>
2018-05-16 00:00:41 +0300

    Generate user scaffold and parse csv file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :index, :create, :update, :delete]
   root 'welcome#index'
+  match '/users/upload', to: 'users#upload', via: 'post', as: 'upload_users'
+  match '/users/new_upload', to: 'users#new_upload', via: 'get', as: 'new_upload_users'
 end
